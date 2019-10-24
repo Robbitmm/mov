e22357855315df8084312ac0a88345123d03d51d
Robbitmm <46769452+Robbitmm@users.noreply.github.com>
2019-10-24 16:57:47 -0300

    Add files via upload

---
+++ Scripts/tempo.js
@@ -0,0 +1,8 @@
+function TimeControl() {
+    let ct = document.getElementsByClassName("ytp-hide-info-bar");
+
+    if (ct.currentTime = 1.41.11) {
+        ct.classList.remove("playing-mode");
+        ct.classList.add("playing-mode")
+    }
+};
