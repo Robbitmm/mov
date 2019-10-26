4758f3daccaeb50bc0920f13855e99dad8aee392
Robbitmm <46769452+Robbitmm@users.noreply.github.com>
2019-10-26 16:20:23 -0300

    Delete tempo.js

--- Scripts/tempo.js
+++
@@ -1,8 +0,0 @@
-function TimeControl() {
-    let ct = document.getElementsByClassName("ytp-hide-info-bar");
-
-    if (ct.currentTime = 1.41.11) {
-        ct.classList.remove("playing-mode");
-        ct.classList.add("playing-mode")
-    }
-};
